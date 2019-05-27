6af110d4d0d223e9214c1a95d2211c6158da653c
Elayan Hamamrah <e.elayan@boundlessdrop.com>
2019-05-28 02:50:21 +0300

    Change from SQLite to Postgres

--- RestOfPeople/settings.py
+++ RestOfPeople/settings.py
@@ -87,9 +87,17 @@ WSGI_APPLICATION = 'RestOfPeople.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'people_on_django',
+        'USER': 'dj_people',
+        'PASSWORD': 'welcome',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
