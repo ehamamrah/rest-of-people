349b6555f57f5bfb265902a76862b19cec193f3f
Elayan Hamamrah <e.elayan@boundlessdrop.com>
2019-05-28 23:27:22 +0300

    People model migrations

--- RestOfPeople/settings.py
+++ RestOfPeople/settings.py
@@ -87,10 +87,6 @@ WSGI_APPLICATION = 'RestOfPeople.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'people_on_django',
--- people/models.py
+++ people/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class People(models.Model):
+  name    = models.CharField('Full Name', max_length = 100, null = False, blank = False)
+  email   = models.CharField('Email', max_length = 100, null = False, blank = False)
+  country = models.CharField('Country', max_length = 100)
+
+  def __str__(self):
+    return self.name
