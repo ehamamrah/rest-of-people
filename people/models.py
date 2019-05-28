2c5c231410e171336cb09e5b35774722bbb9db05
Elayan Hamamrah <e.elayan@boundlessdrop.com>
2019-05-28 23:30:40 +0300

    Added timestamp to people

--- people/models.py
+++ people/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 # Create your models here.
 class People(models.Model):
-  name    = models.CharField('Full Name', max_length = 100, null = False, blank = False)
-  email   = models.CharField('Email', max_length = 100, null = False, blank = False)
-  country = models.CharField('Country', max_length = 100)
+  name      = models.CharField(max_length = 100, null = False, blank = False)
+  email     = models.CharField(max_length = 100, null = False, blank = False)
+  country   = models.CharField(max_length = 100)
+  timestamp = models.DateTimeField(auto_now_add=True)
 
   def __str__(self):
     return self.name
