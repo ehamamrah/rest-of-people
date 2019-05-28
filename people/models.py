0742f88173bee15fa93beafbe002fd607683c0e7
Elayan Hamamrah <e.elayan@boundlessdrop.com>
2019-05-29 01:59:43 +0300

    Email Unique, Requirements File

--- people/models.py
+++ people/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class People(models.Model):
   name      = models.CharField(max_length = 100, null = False, blank = False)
-  email     = models.CharField(max_length = 100, null = False, blank = False)
+  email     = models.CharField(max_length = 100, null = False, blank = False, unique = True)
   country   = models.CharField(max_length = 100)
   timestamp = models.DateTimeField(auto_now_add=True)
 
