924b1cb3b006f219e40fa2437c0632b431acede4
Elayan Hamamrah <e.elayan@boundlessdrop.com>
2019-05-29 22:56:27 +0300

    Change response, Moved to Private Method

--- RestOfPeople/urls.py
+++ RestOfPeople/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import url, include
+from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/people/', include('people.api.urls', namespace = 'api-people-list')),
+    url(r'^api-token/', obtain_jwt_token),
 ]
--- people/api/views.py
+++ people/api/views.py
@@ -2,12 +2,23 @@ from rest_framework import generics
 from people.models import People
 from .serializers import PeopleSerializer
 from .tasks import get_people_list_from_json_file, read_json_file_details
+from rest_framework.response import Response
 import json
 
 class PeopleList(generics.ListAPIView):
   serializer_class = PeopleSerializer
 
-  def get_queryset(self):
+  def list(self, request):
+    queryset   = PeopleList.__people_list_generator()
+    serializer = PeopleSerializer(queryset, many=True)
+    content = {
+      'message': 'success',
+      'data':    serializer.data
+    }
+    return Response(content)
+
+  @classmethod
+  def __people_list_generator(cls):
     list_of_people_from_json = read_json_file_details()
 
     if len(People.objects.all()) < len(list_of_people_from_json):
