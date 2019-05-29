3597afe242ec696cb6bd29c20bc5f18858f745e1
Elayan Hamamrah <e.elayan@boundlessdrop.com>
2019-05-30 00:57:53 +0300

    Bulk Records Insert

--- people/api/tasks.py
+++ people/api/tasks.py
@@ -1,6 +1,7 @@
 import json
 from people.models import People
 from celery import shared_task
+from people import models
 
 @shared_task
 def read_json_file_details():
@@ -11,7 +12,10 @@ def read_json_file_details():
 @shared_task
 def get_people_list_from_json_file():
   list = read_json_file_details()
+  records = []
   for record in list:
-    person = People(name = record[1], email = record[2], country = record[3])
-    person.save()
+    records += [
+      models.People(name = record[1], email = record[2], country = record[3])
+    ]
+  models.People.objects.bulk_create(records)
   return People.objects.all()
